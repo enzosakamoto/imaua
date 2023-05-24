aa215722f479fec1500c96cfc34fcc69a96c2955
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-05-24 16:33:55 -0300

    added createOrder method 🤯

--- src/database/Crud.java
+++ src/database/Crud.java
@@ -10,14 +10,15 @@ import shared.domain.entities.Order;
 
 public class Crud {
     public void createClient(Client client) {
-        String sqlCreateClient = "INSERT INTO clients VALUES (UUID(), ?, ?, ?);";
+        String sqlCreateClient = "INSERT INTO clients VALUES (?, ?, ?, ?);";
         Connection connection = Connector.getConn();
         PreparedStatement stmt = null;
         try {
             stmt = connection.prepareStatement(sqlCreateClient);
-            stmt.setString(1, client.getName());
-            stmt.setString(2, client.getPassword());
-            stmt.setString(3, String.valueOf(client.getCredits()));
+            stmt.setString(1, client.getId());
+            stmt.setString(2, client.getName());
+            stmt.setString(3, client.getPassword());
+            stmt.setString(4, String.valueOf(client.getCredits()));
             stmt.executeUpdate();
             System.out.println("Cliente incluído com sucesso!");
         } catch (SQLException e) {
@@ -32,12 +33,37 @@ public class Crud {
     }
 
     public void createOrder(Order order) {
-        System.out.println("Order created");
+        String sqlCreateOrder = "INSERT INTO orders VALUES (?, ?, ?, ?, ?);";
+        Connection connection = Connector.getConn();
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement(sqlCreateOrder);
+            stmt.setString(1, order.getId());
+            stmt.setString(2, Integer.toString(order.getIdRestaurant()));
+            stmt.setString(3, order.getIdClient());
+            stmt.setString(4, order.getMeal());
+            stmt.setString(5, (order.getIsDone() ? "1" : "0"));
+            stmt.executeUpdate();
+            System.out.println("Pedido incluído com sucesso!");
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                System.out.println("Erro ao incluir os dados do pedido no banco de dados: " + ex.toString());
+            }
+        } finally {
+            Connector.closeConn(connection, stmt);
+        }
     }
 
     public ArrayList<Order> getAllOrdersByClient(String clientId) {
         System.out.println("Getting all orders by client");
         return new ArrayList<Order>();
+    }
+
+    public String getIdClientByName(String name) {
+        System.out.println("Getting id client by name");
+        return "id_client";
     }
 
     public void updateOrder(String orderId) {
--- src/shared/domain/entities/Client.java
+++ src/shared/domain/entities/Client.java
@@ -1,14 +1,22 @@
 package shared.domain.entities;
 
+import java.util.UUID;
+
 public class Client {
+    private String id;
     private String name;
     private String password;
     private double credits;
 
     public Client(String name, String password) {
+        this.id = UUID.randomUUID().toString();
         this.name = name;
         this.password = password;
         this.credits = 0;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -21,6 +29,10 @@ public class Client {
 
     public double getCredits() {
         return credits;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public void setName(String nome) {
